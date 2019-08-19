a9951f95c6b59bc57ee05f27775dbd9b739afa8d
vineeth.t.p <vineeth.t.p@lxisoft.com>
2019-08-19 21:56:14 +0530

    3rd Commit

--- main.c
+++ main.c
@@ -4,7 +4,15 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	int a,b,c;
 	printf("Welcome to LXI");
 	printf("Oh.....");
+	printf("Enter First Number");
+	scanf("%d",&a);
+	printf("Enter Second Number");
+	scanf("%d",&b);
+	c=a+b;
+	printf("\n Sum is %d",c);
+	
 	return 0;
 }
